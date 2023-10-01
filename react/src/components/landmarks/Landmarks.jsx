97066d5a57cca3b28da5e7770156148fad373f79
mnaibei <muchajulius@gmail.com>
2023-10-01 13:27:35 +0300

    Add pagination

--- react/src/components/landmarks/Landmarks.jsx
+++ react/src/components/landmarks/Landmarks.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { fetchLandmarks } from '../../redux/landmarks/apiSlice';
 import IntroHeader from './IntroHeader';
 import SearchComponent from '../searchbar/Search';
@@ -9,104 +10,105 @@ const Landmarks = () => {
   const dispatch = useDispatch();
   const { landmarks } = useSelector((state) => state.landmarks);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [search, setSearch] = useState('');
+  const itemsPerPage = 4;
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     dispatch(fetchLandmarks());
   }, [dispatch]);
 
   useEffect(() => {
-    // Automatically advance the image index
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => {
-        const nextImageIndex = (prevIndex + 1) % landmarks.length;
-
-        console.log('landmarks.length', landmarks.length);
-        console.log('currentImageIndex', prevIndex);
-        console.log('nextImageIndex', nextImageIndex);
-
-        return nextImageIndex;
-      });
-    }, 35000); // Change image every 3 seconds (adjust as needed)
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % landmarks.length);
+    }, 35000);
 
     return () => clearInterval(interval);
   }, [landmarks]);
 
-  // filter landmarks by name
-  const [search, setSearch] = useState('');
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
-  const filterLandmarks = landmarks.filter((landmark) => {
+  const filteredLandmarks = landmarks.filter((landmark) => {
     const landmarkName = landmark.name.toLowerCase();
     const searchTerm = search.toLowerCase();
-
-    // console.log('landmarkName:', landmarkName);
-    // console.log('searchTerm:', searchTerm);
-
     return landmarkName.includes(searchTerm);
   });
+
+  const pageCount = Math.ceil(filteredLandmarks.length / itemsPerPage);
+
+  const paginatedLandmarks = filteredLandmarks.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage,
+  );
 
   return (
     <>
       <IntroHeader />
-      <SearchComponent
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
+      <SearchComponent value={search} onChange={(e) => setSearch(e.target.value)} />
       <div className="p-2 m-2 flex flex-col place-items-center items-center">
-        <div className=" grid grid-cols-2  max-sm:grid-cols-1 items-center  justify-items-center w-full gap-2">
-          {filterLandmarks.map((landmark, index) => (
-
+        <div className="grid grid-cols-2 max-sm:grid-cols-1 items-center justify-items-center w-full gap-2">
+          {paginatedLandmarks.map((landmark, index) => (
             <div
-              // to={`landmark/${landmark.name.replace(/\s+/g, '-')
-              // .toLowerCase()}`} // show landmark name in the url path instead of id
-              // state={landmark}
               key={landmark.id}
               className={`landmarks-card border-2 rounded shadow flex flex-col h-9/12 w-full justify-center items-center p-2 ${index === currentImageIndex ? 'active' : ''
               }`}
             >
               {landmark.images.length > 0 && (
                 <img
-                  src={landmark.images[currentImageIndex
-                    % landmark.images.length]} // Use modulo to ensure index is within bounds
+                  src={landmark.images[currentImageIndex % landmark.images.length]}
                   alt={`${landmark.name} ${currentImageIndex}`}
-                  className="landmark-images border-2 rounded border-black "
+                  className="landmark-images border-2 rounded border-black"
                 />
               )}
-              <div className=" flex flex-col gap-1 w-full p-2
-              "
-              >
-
+              <div className="flex flex-col gap-1 w-full p-2">
                 <h1 className="text-2xl font-extrabold">{landmark.name}</h1>
                 <p>
                   Type:
                   {' '}
                   {landmark.type_of_landmark}
                 </p>
-
                 <p className="desc">
                   Description:
-                  {' '}
                   {landmark.description || 'No info'}
-                  {' '}
-                  <br />
-                  {' '}
                 </p>
-
                 <Link
                   to={`landmark/${landmark.name.replace(/\s+/g, '-').toLowerCase()}`}
                   state={landmark}
                   key={landmark.id}
-                  className="learn-more flex bg-transparent
-                  hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded max-sm:w-full xl:w-2/5 max-lg:w-full justify-center items-center self-end marker:p-1"
+                  className="learn-more flex bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded max-sm:w-full xl:w-2/5 max-lg:w-full justify-center items-center self-end marker:p-1"
                 >
-                  <button type="button">
-                    Learn More
-                  </button>
+                  <button type="button">Learn More</button>
                 </Link>
               </div>
             </div>
-
           ))}
+        </div>
+        <div className="pagination flex items-center mt-4 gap-4">
+          <button
+            onClick={() => handlePageChange((currentPage - 1 + pageCount) % pageCount)}
+            disabled={currentPage === 0}
+            type="button"
+          >
+            <AiOutlineArrowLeft />
+          </button>
+          <span>
+            Page
+            {' '}
+            {currentPage + 1}
+            {' '}
+            of
+            {' '}
+            {pageCount}
+          </span>
+          <button
+            onClick={() => handlePageChange((currentPage + 1) % pageCount)}
+            disabled={currentPage === pageCount - 1}
+            type="button"
+          >
+            <AiOutlineArrowRight />
+          </button>
         </div>
       </div>
     </>
